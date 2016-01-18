e7598b9a6bfee8b4f94b3bf01fc6383ea61b3c28
malaya.pant <malaya.pant@gmail.com>
2016-01-17 23:21:15 -0500

    updated code

--- phoneapp/war/app/app.js
+++ phoneapp/war/app/app.js
@@ -8,13 +8,11 @@ app.controller('personCtrl', function($scope,$http) {
 	
      .success(function(response){$scope.details = response;});
            
-            var lines = $scope.details;
-            
-             var removecomma = lines.substring(0,lines.length - 1); 
-             
+           var lines = $scope.details;
+           var removecomma = lines.substring(0,lines.length - 1); 
            var arr = removecomma.split(',')
            $scope.count=arr.length;
-          $scope.fillarr = arr;
+           $scope.fillarr = arr;
           
     }
 });
